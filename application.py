49586ec359d4c6f8893cdf22cec7929fb802c03f
Muzamil Rajper <muzamilrajper@outlook.com>
2025-12-18 15:21:19 +0500

    Clean up application.py by removing commented code
    Removed commented-out code related to Flask application setup and model loading.

--- application.py
+++ application.py
@@ -47,37 +47,3 @@ def predict_datapoint():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
 
-
-# import pickle
-
-# from flask import Flask,request,jsonify,render_template
-# import numpy as np
-# import pandas as pd
-# from sklearn.preprocessing import StandardScaler
-
-
-# # # import ridge regressor and standard scaler pickle
-# ridge_model=pickle.load(open('models/ridge.pkl','rb'))
-# standard_scaler=pickle.load(open('models/scaler.pkl','rb'))
-
-# application = Flask(__name__)
-# app=application
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-
-# @app.route('/predictdata',methods=['GET', 'POST'])
-# def predict_datapoint():
-#     if request.method=='POST':
-#         pass
-#     else:
-#         return render_template('home.html')
-    
-
-
-#     # return "<h1>Flask is working! This is not a blank page.</h1>"
-
-
-# if __name__=="__main__":
-#     app.run(host="0.0.0.0", port=8000)
-
